78e4dc6f2220dd694c995298471d64077ef0efa6
fpaiz-2022134 <fpaiz-2022134@kinal.edu.gt>
2024-04-11 20:24:53 -0600

    Estructura de proyecto.

---
+++ HotelManagement-KinalTec/configs/app.js
@@ -0,0 +1,35 @@
+//Imports
+import express from 'express'
+import cors from 'cors'
+import helmet from 'helmet'
+import morgan from 'morgan'
+import { config } from 'dotenv'
+
+
+
+
+//Configuration
+const app = express()
+config()
+
+const port = process.env.PORT || 3200
+
+//Configurating the server 
+//(Configurando el servidor de express)
+
+app.use(express.urlencoded({ extended: false}))
+app.use(express.json)
+app.use(cors())
+app.use(helmet())
+app.use(morgan('dev'))
+
+//Routes
+
+
+
+//Levantamos el servidor
+
+export const initServer = ()=>{
+    app.listen(port)
+    console.log(`Server HTTP running in port ${port}`)
+}
