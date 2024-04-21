ff477e2c9de99578af3aac6fea909f7ada717190
elutin-2019276 <elutin-2019276@kinal.edu.gt>
2024-04-20 21:12:51 -0600

    Se trabajo en el crud de evento.

--- HotelManagement-KinalTec/src/event/event.routes.js
+++ HotelManagement-KinalTec/src/event/event.routes.js
@@ -4,18 +4,22 @@ import { Router } from 'express'
 
 import {
     saveEvent,
-    test
+    test,
+    updateEvent,
+    changeStatus
 } from './event.controller.js'
 
+import {
+    validateJwt,
+    isAdmin,
+} from '../middlewares/validate-jwt.js'
 
 const api = Router()
 
-api.post('/saveEvent', saveEvent)
-
-
-//ROLE CLIENT/ADMIN
-//api.get('/get', [validateJwt], get)
-//api.post('/search', [validateJwt], search)
+api.post('/saveEvent', saveEvent,)
+api.put('/updateEvent/:id', [validateJwt, isAdmin], updateEvent)
+api.put('/changeStatus/:id', [validateJwt, isAdmin], changeStatus)
 
 api.get('/test', test)
+
 export default api
